88eb50ef0a8f5454962b01efde98a0a6417b2d10
bowtieman143 <steven@onloaddevelopment.com>
2020-10-17 14:25:47 -0700

    Ready MVP

--- includes/modules/HelloWorld/HelloWorld.jsx
+++ includes/modules/HelloWorld/HelloWorld.jsx
@@ -1,23 +1,31 @@
 // External Dependencies
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 // Internal Dependencies
 import './style.css';
 
 
-class HelloWorld extends Component {
+class SimpleHeader extends Component {
 
-  static slug = 'myex_hello_world';
+  static slug = 'simp_simple_header';
 
   render() {
-    const Content = this.props.content;
-
     return (
-      <h1>
-        <Content/>
-      </h1>
+      <Fragment>
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={this.props.header_image} alt="Avatar" style={{width:"300px",height:"300px"}} />
+            </div>
+            <div class="flip-card-back">
+              <h1>{this.props.heading}</h1> 
+              <p>{this.props.content()}</p>
+            </div>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
 
-export default HelloWorld;
+export default SimpleHeader;
--- includes/modules/HelloWorld/HelloWorld.php
+++ includes/modules/HelloWorld/HelloWorld.php
@@ -1,35 +1,60 @@
 <?php
 
-class MYEX_HelloWorld extends ET_Builder_Module {
+class SIMP_SimpleHeader extends ET_Builder_Module {
 
-	public $slug       = 'myex_hello_world';
+	public $slug       = 'simp_simple_header';
 	public $vb_support = 'on';
 
-	protected $module_credits = array(
-		'module_uri' => 'onloaddevelopment.com',
-		'author'     => 'Steven Peralta',
-		'author_uri' => 'onloaddevelopment.com',
-	);
-
 	public function init() {
-		$this->name = esc_html__( 'Hello World', 'myex-my-extension' );
+		$this->name = esc_html__( 'Simple Header', 'simp-simple-extension' );	
 	}
 
 	public function get_fields() {
 		return array(
-			'content' => array(
-				'label'           => esc_html__( 'Content', 'myex-my-extension' ),
+			'header_image'        => array(
+				'label'           => esc_html__( 'Heading', 'simp-simple-extension' ),
+				'type'            => 'upload',
+				'option_category' => 'basic_option',
+				'description'     => esc_html__( 'Input your desired heading here.', 'simp-simple-extension' ),
+				'toggle_slug'     => 'main_content',
+			),
+			'heading'     => array(
+				'label'           => esc_html__( 'Heading', 'simp-simple-extension' ),
+				'type'            => 'text',
+				'option_category' => 'basic_option',
+				'description'     => esc_html__( 'Input your desired heading here.', 'simp-simple-extension' ),
+				'toggle_slug'     => 'main_content',
+			),
+			'content'     => array(
+				'label'           => esc_html__( 'Content', 'simp-simple-extension' ),
 				'type'            => 'tiny_mce',
 				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Content entered here will appear inside the module.', 'myex-my-extension' ),
+				'description'     => esc_html__( 'Content entered here will appear below the heading text.', 'simp-simple-extension' ),
 				'toggle_slug'     => 'main_content',
 			),
 		);
 	}
 
-	public function render( $attrs, $content = null, $render_slug ) {
-		return sprintf( '<h1>%1$s</h1>', $this->props['content'] );
+	public function render( $unprocessed_props, $content = null, $render_slug ) {
+		return sprintf(
+			'
+			<div class="flip-card">
+				<div class="flip-card-inner">
+					<div class="flip-card-front">
+						<img src="%1$s" alt="Avatar" style="width:300px;height:300px;">
+					</div>
+					<div class="flip-card-back">
+						<h1>%2$s</h1> 
+						<p>%3$s</p>
+					</div>
+				</div>
+			</div>
+			',
+			esc_html( $this->props['header_image'] ),
+			esc_html( $this->props['heading'] ),
+			$this->props['content']
+		);
 	}
 }
 
-new MYEX_HelloWorld;
+new SIMP_SimpleHeader;
